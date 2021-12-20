2d8fe4684b00d01d363c8b3fda6bf97d1ff1bbcd
Hong Yu Yi <you@example.com>
2021-12-20 23:37:55 +1100

    Game start/reset created

--- index.js
+++ index.js
@@ -1,8 +1,70 @@
 //Variables
 const undo = document.querySelector("[data-undo]")
 const reset = document.querySelector("[data-reset]")
+const playArea = document.querySelectorAll(".play-area")
+let takenTiles = []
+let randomNumber = 0
 
 //Functions
+let start = () => {
+  for (let i = 0; i < 2; i++) {
+    randomNumber = Math.floor(Math.random() * 2)
+    createBlock(randomNumber)
+  }
+
+  console.log("Game started/restarted")
+}
+
+function clearCanvas() {
+  takenTiles = []
+
+  for (let i in playArea) {
+    let block = playArea[i].children
+
+    if (block === undefined) break
+
+    if (block.length != 0) {
+      block[0].remove()
+    }
+  }
+}
+
+function createBlock(type) {
+  //Make sure both blocks are not in the same area
+  let exit = true
+
+  do {
+    exit = true
+    randomNumber = Math.floor(Math.random() * playArea.length)
+
+    for (let i in takenTiles) {
+      if (takenTiles[i] == randomNumber) {
+        exit = false
+      }
+    }
+  } while (!exit)
+
+  takenTiles.push(randomNumber)
+
+  //2
+  if (type == 0) {
+    const two = document.createElement("div")
+    two.innerText = "2"
+    two.classList.add("play-area")
+    two.classList.add("block")
+    two.classList.add("two")
+    playArea[randomNumber].append(two)
+  }
+  //4
+  if (type == 1) {
+    const four = document.createElement("div")
+    four.innerText = "4"
+    four.classList.add("play-area")
+    four.classList.add("block")
+    four.classList.add("four")
+    playArea[randomNumber].append(four)
+  }
+}
 
 //Onclick
 undo.onclick = () => {
@@ -10,7 +72,8 @@ undo.onclick = () => {
 }
 
 reset.onclick = () => {
-  console.log("Reset the ting")
+  clearCanvas()
+  start()
 }
 
 //On keyboard
@@ -25,3 +88,6 @@ document.addEventListener("keydown", (event) => {
     console.log("Go up boi")
   }
 })
+
+//Game logic
+start()
