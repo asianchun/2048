0408826625d95e2b7ab15695bfe85db0e581ccbc
Hong Yu Yi <you@example.com>
2021-12-21 18:15:20 +1100

    Added functionality

--- index.js
+++ index.js
@@ -2,21 +2,34 @@
 const undo = document.querySelector("[data-undo]")
 const reset = document.querySelector("[data-reset]")
 const playArea = document.querySelectorAll(".play-area")
+const score = document.querySelector(".score")
 let takenTiles = []
-let randomNumber = 0
+let lose = false
+//0,1,2,3 / 4,5,6,7 / 8,9,10,11 / 12,13,14,15 right
+//0,4,8,12 / 1,5,9,13 / 2,6,10,14 / 3,7,11,15 down
+//3,2,1,0 / 7,6,5,4 / 11,10,9,8 / 15,14,13,12 left
+//12,8,4,0 / 13,9,5,1 / 14,10,6,2 / 15,11,7,3 up
 
 //Functions
 let start = () => {
   for (let i = 0; i < 2; i++) {
-    randomNumber = Math.floor(Math.random() * 2)
-    createBlock(randomNumber)
+    createBlock()
   }
 
   console.log("Game started/restarted")
 }
 
+function checkLoss() {
+  if (takenTiles.length == playArea.length) {
+    lose = true
+    score.append(" You lost!")
+  }
+}
+
 function clearCanvas() {
   takenTiles = []
+  lose = false
+  score.innerText = "Score: 0"
 
   for (let i in playArea) {
     let block = playArea[i].children
@@ -29,10 +42,12 @@ function clearCanvas() {
   }
 }
 
-function createBlock(type) {
-  //Make sure both blocks are not in the same area
+function createBlock() {
   let exit = true
+  let type = Math.floor(Math.random() * 2)
+  let randomNumber = 0
 
+  //Make sure both blocks are not in the same area
   do {
     exit = true
     randomNumber = Math.floor(Math.random() * playArea.length)
@@ -45,6 +60,7 @@ function createBlock(type) {
   } while (!exit)
 
   takenTiles.push(randomNumber)
+  checkLoss()
 
   //2
   if (type == 0) {
@@ -78,13 +94,19 @@ reset.onclick = () => {
 
 //On keyboard
 document.addEventListener("keydown", (event) => {
+  if (lose) return
+
   if (event.key == "ArrowLeft") {
+    createBlock()
     console.log("Go left boi")
   } else if (event.key == "ArrowRight") {
+    createBlock()
     console.log("Go right boi")
   } else if (event.key == "ArrowDown") {
+    createBlock()
     console.log("Go down boi")
   } else if (event.key == "ArrowUp") {
+    createBlock()
     console.log("Go up boi")
   }
 })
