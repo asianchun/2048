ca8cb6c416a40abcb2a632828cd0da854a676f0b
Hong Yu Yi <you@example.com>
2021-12-28 00:10:35 +1100

    First iteration of combine block

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ const reset = document.querySelector("[data-reset]")
 const playArea = document.querySelectorAll(".play-area")
 const score = document.querySelector(".score")
 const progression = [
-  ".two",
-  ".four",
-  ".eight",
-  ".sixteen",
-  ".thirty-two",
-  ".sixty-four",
-  ".one-two-eight",
-  ".two-five-six",
-  ".five-one-two",
-  ".one-thousand-two-four",
-  ".two-thousand-four-eight",
+  "two",
+  "four",
+  "eight",
+  "sixteen",
+  "thirty-two",
+  "sixty-four",
+  "one-two-eight",
+  "two-five-six",
+  "five-one-two",
+  "one-thousand-two-four",
+  "two-thousand-four-eight",
+]
+const numericProgression = [
+  "2", //0
+  "4", //1
+  "8", //2
+  "16", //3
+  "32", //4
+  "64", //5
+  "128", //6
+  "256", //7
+  "512", //8
+  "1024", //9
+  "2048", //10
 ]
 const right = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 const left = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
@@ -53,8 +66,6 @@ function clearCanvas() {
 
   for (let area of playArea) {
     let block = area.children
-
-    if (block === undefined) break
 
     if (block.length != 0) block[0].remove()
   }
@@ -97,9 +108,51 @@ function createBlock() {
   }
 }
 
-//TODO: Create the ability to combine blocks using the progression
-function combineBlock() {
-  console.log("blocks combined")
+//TODO: DEBUGGGGG
+function combineBlock(direction) {
+  /*
+   * Left will use right, up will use down and vice versa
+   * This is the only way to combine properly
+   */
+  switch (direction) {
+    case "right":
+      console.log("blocks combined right")
+      break
+    case "left":
+      for (let i = 0; i < right.length; i++) {
+        if (i == right.length - 1) break
+
+        let block = playArea[right[i]].children
+        let nextBlock = playArea[right[i + 1]].children
+        let index = 0
+
+        if (block.length != 0 && nextBlock.length != 0) {
+          if (block[0].classList.item(2) == nextBlock[0].classList.item(2)) {
+            for (let i = 0; i < progression.length; i++) {
+              if (progression[i] == block[0].classList.item(2)) index = i
+            }
+
+            block[0].innerText = numericProgression[index + 1]
+            block[0].classList.replace(
+              progression[index],
+              progression[index + 1]
+            )
+            nextBlock[0].remove()
+          }
+        }
+      }
+
+      // * Need this -> ar[3] || ar[7] || ar[11] || ar[15]
+      break
+    case "down":
+      console.log("blocks combined down")
+      break
+    case "up":
+      console.log("blocks combined up")
+      break
+    default:
+      console.log("Something went wrong")
+  }
 }
 
 function countIterations(ar, i, n, spread, operator) {
@@ -229,7 +282,7 @@ function countIterations(ar, i, n, spread, operator) {
   }
 }
 
-function moveBlock(direction, array) {
+function moveBlock(direction) {
   //Control whether new block gets created or not
   let previousState = []
   let equal = true
@@ -252,7 +305,7 @@ function moveBlock(direction, array) {
 
   //Move each block accordingly
   for (let i in takenTiles) {
-    let tile = playArea[takenTiles[i]].childNodes
+    let tile = playArea[takenTiles[i]].children
     iteration = 3
     success = false
 
@@ -261,19 +314,19 @@ function moveBlock(direction, array) {
       switch (direction) {
         case "right":
           if (takenTiles[i] < takenTiles[n] || i == 0)
-            countIterations(array, i, n, 1, "+")
+            countIterations(right, i, n, 1, "+")
           break
         case "left":
           if (takenTiles[i] > takenTiles[n] || i == 0)
-            countIterations(array, i, n, 1, "-")
+            countIterations(left, i, n, 1, "-")
           break
         case "down":
           if (takenTiles[i] < takenTiles[n] || i == 0)
-            countIterations(array, i, n, 4, "+")
+            countIterations(down, i, n, 4, "+")
           break
         case "up":
           if (takenTiles[i] > takenTiles[n] || i == 0)
-            countIterations(array, i, n, 4, "-")
+            countIterations(up, i, n, 4, "-")
           break
         default:
           console.log("Something went wrong")
@@ -283,19 +336,19 @@ function moveBlock(direction, array) {
     //Make the movement
     switch (direction) {
       case "right":
-        playArea[takenTiles[i] + iteration].appendChild(tile[0])
+        playArea[takenTiles[i] + iteration].append(tile[0])
         takenTiles[i] += iteration
         break
       case "left":
-        playArea[takenTiles[i] - iteration].appendChild(tile[0])
+        playArea[takenTiles[i] - iteration].append(tile[0])
         takenTiles[i] -= iteration
         break
       case "down":
-        playArea[takenTiles[i] + 4 * iteration].appendChild(tile[0])
+        playArea[takenTiles[i] + 4 * iteration].append(tile[0])
         takenTiles[i] += 4 * iteration
         break
       case "up":
-        playArea[takenTiles[i] - 4 * iteration].appendChild(tile[0])
+        playArea[takenTiles[i] - 4 * iteration].append(tile[0])
         takenTiles[i] -= 4 * iteration
         break
       default:
@@ -303,6 +356,7 @@ function moveBlock(direction, array) {
     }
   }
 
+  //TODO: Move this after combineBlock()
   //Check whether the game is over / play area is full
   checkGameOver(direction)
 
@@ -316,6 +370,7 @@ function moveBlock(direction, array) {
 
 //New game
 reset.onclick = () => {
+  console.clear()
   clearCanvas()
   start()
 }
@@ -323,13 +378,14 @@ reset.onclick = () => {
 //On keyboard
 document.addEventListener("keydown", (event) => {
   if (event.key == "ArrowLeft") {
-    moveBlock("left", left)
+    moveBlock("left")
+    combineBlock("left")
   } else if (event.key == "ArrowRight") {
-    moveBlock("right", right)
+    moveBlock("right")
   } else if (event.key == "ArrowDown") {
-    moveBlock("down", down)
+    moveBlock("down")
   } else if (event.key == "ArrowUp") {
-    moveBlock("up", up)
+    moveBlock("up")
   }
 })
 
